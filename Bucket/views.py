d9f6309bc15171af33e62adaefd8d37c1d85ce7e
michaelrsullivan1 <michaelrsullivan1@gmail.com>
2014-10-29 11:29:47 -0500

    Fixed render_to_response issue and updated Navbar

--- Bucket/views.py
+++ Bucket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response, render
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.core.context_processors import csrf
@@ -7,22 +7,23 @@ from BucketList.forms import UserProfileForm
 from BucketList.models import UserProfile
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+import operator
 
 
 def login(request):
     #The user login page
     c = {}
     c.update(csrf(request))
-    return render_to_response('login.html', c)
+    return render(request, 'login.html', c)
     
 def auth_view(request):
     #Authenticates to make sure the information is correct
     username = request.POST.get('username', ' ')
     password = request.POST.get('password', ' ')
-    user = auth.authenticate(username=username, password=password)
+    user1 = auth.authenticate(username=username, password=password)
     
-    if user is not None:
-        auth.login(request, user)
+    if user1 is not None:
+        auth.login(request, user1)
         return HttpResponseRedirect('/accounts/loggedin')
     else:
         return HttpResponseRedirect('/accounts/invalid')
@@ -39,12 +40,12 @@ def loggedin(request):
                                               
 def invalid_login(request):
     #Screen displayed if there was a problem with the login information
-    return render_to_response('invalid_login.html')
+    return render(request, 'invalid_login.html')
     
 def logout(request):
     #Screen displayed if user is logged out
     auth.logout(request)
-    return render_to_response('logout.html')
+    return render(request, 'logout.html')
     
 def register_user(request):
     #The view that allows the user to register
@@ -57,12 +58,12 @@ def register_user(request):
     args = {}
     args.update(csrf(request))
     args['form'] = MyRegistrationForm()
-    return render_to_response('register.html', args)
+    return render(request, 'register.html', args)
     
     
 def register_success(request):
     #Screen returned if registration was a success
-    return render_to_response('register_success.html')
+    return render(request, 'register_success.html')
     
     
 @login_required        
@@ -71,14 +72,14 @@ def edit(request):
     if request.method == 'POST':
         form = EditRegistrationForm(request.POST)
         if form.is_valid():
-            user = User.objects.get(id = request.user.id)
-            username = user.username
+            user1 = User.objects.get(id = request.user.id)
+            username = user1.username
             email = form.cleaned_data['email']
             new_email = form.cleaned_data['new_email']
             password = form.cleaned_data['password']
             new_password1 = form.cleaned_data['new_password1']               
             new_password2 = form.cleaned_data['new_password2']
-            email_match = email == user.email
+            email_match = email == user1.email
             pass_match = auth.authenticate(username = username, password = password)
             change_email = False
             change_password = False
@@ -93,33 +94,33 @@ def edit(request):
                         try:
                             exists = User.objects.get(email = new_email)
                             args['email_not_unique'] = True
-                            return render_to_response('edit_success.html', args)
+                            return render(request, 'edit_success.html', args)
                         except:
-                            user.email = new_email
-                            user.save() 
+                            user1.email = new_email
+                            user1.save() 
                             change_email = True
                     else:
                         args['no_email_match'] = True
-                        return render_to_response('edit_success.html', args)
+                        return render(reqeust, 'edit_success.html', args)
             
                 if new_password1 and new_password1 == new_password2:
-                    user.set_password(new_password1)
-                    user.save()
+                    user1.set_password(new_password1)
+                    user1.save()
                     change_password = True                                
                  
                 if change_email or change_password:
                     
                     args['change_email'] = change_email
                     args['change_password'] = change_password
-                    return render_to_response('edit_success.html', args)
+                    return render(request, 'edit_success.html', args)
             else:
                 args['pass_match'] = pass_match
-                return render_to_response('edit_success.html', args)
+                return render(request, 'edit_success.html', args)
                     
     args = {}
     args.update(csrf(request))
     args['form'] = EditRegistrationForm()
-    return render_to_response('edit.html', args)
+    return render(request, 'edit.html', args)
  
     
     
